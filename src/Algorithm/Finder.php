588ddbf17b0ff5616eb039f49d1a2b9a754d4947
Rakzodia <jeec.estaba@gmail.com>
2018-12-27 03:56:18 +0000

    [refactor] Finder

--- src/Algorithm/Finder.php
+++ src/Algorithm/Finder.php
@@ -6,60 +6,65 @@ namespace CodelyTV\FinderKata\Algorithm;
 
 final class Finder
 {
-    /** @var Thing[] */
-    private $_p;
+    private $_personList;
+    private $_birthDateCompareList;
 
-    public function __construct(array $p)
-    {
-        $this->_p = $p;
+    public function __construct(array $personList) {
+        $this->_personList = $personList;
     }
 
-    public function find(int $ft): F
-    {
-        /** @var F[] $tr */
-        $tr = [];
-
-        for ($i = 0; $i < count($this->_p); $i++) {
-            for ($j = $i + 1; $j < count($this->_p); $j++) {
-                $r = new F();
-
-                if ($this->_p[$i]->birthDate < $this->_p[$j]->birthDate) {
-                    $r->p1 = $this->_p[$i];
-                    $r->p2 = $this->_p[$j];
-                } else {
-                    $r->p1 = $this->_p[$j];
-                    $r->p2 = $this->_p[$i];
-                }
-
-                $r->d = $r->p2->birthDate->getTimestamp()
-                    - $r->p1->birthDate->getTimestamp();
-
-                $tr[] = $r;
-            }
+    public function find(int $diference): BirthDateCompare {
+        $this->_birthDateCompareList = $this->getBirthDateCompareList();
+        
+        if (!count($this->_birthDateCompareList)) {
+            return new BirthDateCompare();
         }
 
-        if (count($tr) < 1) {
-            return new F();
-        }
+        return $this->getBirthdateCompareBy($diference);
+    }
+    
+    private function getBirthdateCompareBy($diference) {
+        $firstBirthDateCompare = $this->_birthDateCompareList[0];
 
-        $answer = $tr[0];
-
-        foreach ($tr as $result) {
-            switch ($ft) {
-                case FT::ONE:
-                    if ($result->d < $answer->d) {
-                        $answer = $result;
+        foreach ($this->_birthDateCompareList as $birthdayCompare) {
+            switch ($diference) {
+                case Diference::NEAR:
+                    if ($birthdayCompare->diferenceDays < $firstBirthDateCompare->diferenceDays) {
+                        $firstBirthDateCompare = $birthdayCompare;
                     }
                     break;
 
-                case FT::TWO:
-                    if ($result->d > $answer->d) {
-                        $answer = $result;
+                case Diference::FAR:
+                    if ($birthdayCompare->diferenceDays > $firstBirthDateCompare->diferenceDays) {
+                        $firstBirthDateCompare = $birthdayCompare;
                     }
                     break;
             }
         }
+        
+        return $firstBirthDateCompare;
+    }
+    
+    private function getBirthDateCompareList() {
+        $birthDate = [];
+        
+        $personNumbers = count($this->_personList);
+        
+        for ($i = 0; $i < $personNumbers; $i++) {
+            for ($j = $i + 1; $j < $personNumbers; $j++) {
+                $r = new BirthDateCompare();
+                
+                $isTheFirstPersonMinor = $this->_personList[$i]->birthDate < $this->_personList[$j]->birthDate;
+                
+                $r->person1 = $this->_personList[ $isTheFirstPersonMinor ? $i : $j ];
+                $r->person2 = $this->_personList[ $isTheFirstPersonMinor ? $j : $i];
 
-        return $answer;
+                $r->diferenceDays = $r->person2->birthDate->getTimestamp() - $r->person1->birthDate->getTimestamp();
+
+                $birthDate[] = $r;
+            }
+        }
+        
+        return $birthDate;
     }
 }
